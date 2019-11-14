8cc936805ae31599f9e1ea245c3df19ad2f67ed3
Amaterixen <joshjcassidy@hotmail.com>
2019-11-14 15:01:24 +0000

    login and logout endpoints

--- src/main/java/com/mycompany/bankingapplication/CustomerResource.java
+++ src/main/java/com/mycompany/bankingapplication/CustomerResource.java
@@ -16,6 +16,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.CookieParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
@@ -25,7 +26,9 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.PathParam; 
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Cookie;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.NewCookie;
 import javax.ws.rs.core.Response;
 
 /**
@@ -35,6 +38,34 @@ import javax.ws.rs.core.Response;
 @Path("/customer")
 public class CustomerResource {
     CustomersDataService custOp = CustomersDataService.getInstance();
+    
+    
+    @POST
+    @Path("/login")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response login(Customer customer){
+        String securityCred = customer.getSecurityCred();
+        Customer loginCustomer = CustomersDataService.getInstance().getCustomerBySecurityCred(securityCred);
+        if(!loginCustomer.equals(null)){
+            String loginId = loginCustomer.getId();
+            NewCookie cookie = new NewCookie("customerId", loginId);
+            return Response.status(Response.Status.OK).cookie(cookie).build();
+        }
+        return Response.status(Response.Status.NOT_FOUND).entity("Customer Does Not Exist").build();
+    }
+    
+    @POST
+    @Path("/logout")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response logout(@CookieParam("customerId") Cookie cookie){
+        if(cookie != null){
+            NewCookie newCookie = new NewCookie(cookie, null, 0, false);
+            return Response.status(Response.Status.OK).cookie(newCookie).build();
+        }
+        return Response.status(Response.Status.OK).entity("OK - No session").build();
+    }
     
     @POST
     @Path("/createcustomer")
